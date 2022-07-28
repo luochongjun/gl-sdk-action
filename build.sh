ac7c9bf46c9bc434c711658715d7a69551d78b41
gl-luochongjun <luochongjun@gl-inet.com>
2022-07-28 19:43:26 +0800

    update

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ WORKDIR="$(pwd)"
 sudo -E apt-get update
 sudo -E apt-get install git  asciidoc bash bc binutils bzip2 fastjar flex gawk gcc genisoimage gettext git intltool jikespg libgtk2.0-dev libncurses5-dev libssl1.0-dev make mercurial patch perl-modules python2.7-dev rsync ruby sdcc subversion unzip util-linux wget xsltproc zlib1g-dev zlib1g-dev -y
 
+git config --global user.email "aa@163.com"
+git config --global user.name "aa"
+
 git clone "$SOURCECODEURL" buildsource
 git clone git@github.com:gl-inet-builder/openwrt-sdk-siflower-1806.git openwrt-sdk
 cd openwrt-sdk
