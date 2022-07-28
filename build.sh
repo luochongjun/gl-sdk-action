c6c46a1b523fa5d640453c1e09eb0007d3ed4780
gl-luochongjun <luochongjun@gl-inet.com>
2022-07-28 19:33:44 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 echo SOURCECODEURL: "$SOURCECODEURL"
 echo PKGNAME: "$PKGNAME"
+
+WORKDIR="$(pwd)"
+
+apt update
+apt install git  asciidoc bash bc binutils bzip2 fastjar flex gawk gcc genisoimage gettext git intltool jikespg libgtk2.0-dev libncurses5-dev libssl1.0-dev make mercurial patch perl-modules python2.7-dev rsync ruby sdcc subversion unzip util-linux wget xsltproc zlib1g-dev zlib1g-dev -y
+
+git clone "$SOURCECODEURL" buildsource
+git clone git@github.com:gl-inet-builder/openwrt-sdk-siflower-1806.git openwrt-sdk
+cd openwrt-sdk
+sed -i "1i\src-link local ${WORKDIR}/buildsource" feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+echo CONFIG_ALL=y >.config
+make defconfig
+make V=s ./package/${PKGNAME}/compile
