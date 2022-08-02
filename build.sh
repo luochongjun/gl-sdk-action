09bfe73219698223a5c5d2349b4f48ce321c424c
gl-luochongjun <luochongjun@gl-inet.com>
2022-08-02 12:34:03 +0800

    push

--- build.sh
+++ build.sh
@@ -2,14 +2,18 @@
 echo SOURCECODEURL: "$SOURCECODEURL"
 echo PKGNAME: "$PKGNAME"
 echo BOARD: "$BOARD"
+EMAIL=${EMAIL:-"aa@163.com"}
+echo EMAIL: "$EMAIL"
+echo PASSWORD: "$PASSWORD"
 
 WORKDIR="$(pwd)"
 
 sudo -E apt-get update
 sudo -E apt-get install git  asciidoc bash bc binutils bzip2 fastjar flex gawk gcc genisoimage gettext git intltool jikespg libgtk2.0-dev libncurses5-dev libssl1.0-dev make mercurial patch perl-modules python2.7-dev rsync ruby sdcc subversion unzip util-linux wget xsltproc zlib1g-dev zlib1g-dev -y
 
-git config --global user.email "aa@163.com"
+git config --global user.email "${EMAIL}"
 git config --global user.name "aa"
+[ -n "${PASSWORD}" ] && git config --global user.password "${PASSWORD}"
 
 mkdir -p  ${WORKDIR}/buildsource
 cd  ${WORKDIR}/buildsource
@@ -43,12 +47,12 @@ case "$BOARD" in
 esac
 
 cd openwrt-sdk
-sed -i "1i\src-link local ${WORKDIR}/buildsource" feeds.conf.default
+sed -i "1i\src-link githubaction ${WORKDIR}/buildsource" feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 echo CONFIG_ALL=y >.config
 make defconfig
-make V=s ./package/feeds/local/${PKGNAME}/compile
+make V=s ./package/feeds/githubaction/${PKGNAME}/compile
 
 find bin -type f -exec ls -lh {} \;
 find bin -type f -name "*.ipk" -exec cp -f {} "${WORKDIR}" \; 
